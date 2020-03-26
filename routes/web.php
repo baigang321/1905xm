da976145f1113546682c680fe6bdbdc74bc1709a
baigang <3255728708@qq.com>
2020-03-26 07:48:15 +0000

    a

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
     Route::get('login/register','Login\LoginController@register');
     Route::post('login/registerto','Login\LoginController@registerto');
     Route::get('login/login','Login\LoginController@Login');
     Route::post('login/loginto','Login\LoginController@Loginto');
-    Route::any('index/index','Login\IndexController@index');
+    Route::any('/','Login\IndexController@index');
 
